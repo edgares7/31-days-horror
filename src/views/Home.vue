f0f0a7814ab46d58e6bd186429b898ff6c2b7812
Edgar Esparza <eesparza@dimin.com>
2021-08-31 09:38:54 -0500

    initial commit

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,31 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js App" />
+    <div class="calendar">
+      <div class="calendar__header">
+        <h1>October</h1>
+      </div>
+      <div class="calendar__body">
+        <div class="calendar__body--days flex flex-wrap">
+          <div
+            v-for="(day, i) in days"
+            :key="i"
+            class="day border-solid border-gray-300 border w-40 h-40"
+          >
+            <p>{{ day }}</p>
+          </div>
+        </div>
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from "@/components/HelloWorld.vue";
-
 export default {
   name: "Home",
-  components: {
-    HelloWorld,
+  data() {
+    return {
+      days: 31,
+    };
   },
 };
 </script>
